ca4332a41d1e0542ef1bab41ef395838ac1636dc
Sherafgan Khan <97927727+sherafyk@users.noreply.github.com>
2025-05-30 15:12:20 -0700

    Update admonitions.js

--- assets/admonitions.js
+++ assets/admonitions.js
@@ -1,21 +1,15 @@
-
-
-
-/*
-
 document.querySelectorAll('.markdown-body blockquote').forEach(blockquote => {
   const text = blockquote.textContent.trim();
   if (text.startsWith('[!NOTE]')) {
-    blockquote.classList.add('admonition', 'note');
-    blockquote.innerHTML = blockquote.innerHTML.replace('[!NOTE]', '<strong>ⓘ Note</strong><br>');
+    blockquote.classList.add('markdown-alert', 'markdown-alert-note');
+    blockquote.innerHTML = blockquote.innerHTML.replace('[!NOTE]', '<strong>ⓘ Note:</strong>');
   }
   if (text.startsWith('[!WARNING]')) {
-    blockquote.classList.add('admonition', 'warning');
-    blockquote.innerHTML = blockquote.innerHTML.replace('[!WARNING]', '');
+    blockquote.classList.add('markdown-alert', 'markdown-alert-warning');
+    blockquote.innerHTML = blockquote.innerHTML.replace('[!WARNING]', '<strong>⚠️ Warning:</strong>');
   }
   if (text.startsWith('[!TIP]')) {
-    blockquote.classList.add('admonition', 'tip');
-    blockquote.innerHTML = blockquote.innerHTML.replace('[!TIP]', '');
+    blockquote.classList.add('markdown-alert', 'markdown-alert-tip');
+    blockquote.innerHTML = blockquote.innerHTML.replace('[!TIP]', '<strong>💡 Tip:</strong>');
   }
 });
-
